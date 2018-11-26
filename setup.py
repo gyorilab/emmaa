9c5e2bd4b56ff2f8156d314394161672c23a72a6
Ben Gyori <ben.gyori@gmail.com>
2018-11-26 09:44:17 -0500

    Add initial setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from os import path
+from setuptools import setup, find_packages
+
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(name='emmaa',
+      version='1.0.0',
+      description='Ecosystem of Machine-maintained Models with ' + \
+                  'Automated Assembly',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/indralab/emmaa',
+      author='EMMAA developers, Harvard Medical School',
+      author_email='benjamin_gyori@hms.harvard.edu',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3'
+        ],
+      packages=find_packages(),
+      install_requires=['indra']
+      )
