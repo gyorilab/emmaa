d8352b010d47b2698e8abab08c896bde7c2b4125
Ben Gyori <ben.gyori@gmail.com>
2018-12-03 12:10:28 -0500

    Add docstrings to prior statement retrieval

--- emmaa/priors/prior_stmts.py
+++ emmaa/priors/prior_stmts.py
@@ -3,11 +3,38 @@ from indra.tools import assemble_corpus as ac
 
 
 def get_stmts_for_gene(gene):
+    """Return all existing Statements for a given gene from the DB.
+
+    Parameters
+    ----------
+    gene : str
+        The HGNC symbol of a gene to query.
+
+    Returns
+    -------
+    list[indra.statements.Statement]
+        A list of INDRA Statements in which the given gene is involved.
+    """
     return client.get_statements_by_gene_role_type(gene, preassembled=False,
                                                    count=100000)
 
 
 def get_stmts_for_gene_list(gene_list, other_entities):
+    """Return all Statements between genes in a given list.
+
+    Parameters
+    ----------
+    gene_list : list[str]
+        A list of HGNC symbols for genes to query.
+    other_entities : list[str]
+        A list of other entities to keep as part of the set of Statements.
+
+    Returns
+    -------
+    list[indra.statements.Statement]
+        A list of INDRA Statements between the given list of genes and other
+        entities specified.
+    """
     stmts = []
     for gene in gene_list:
         stmts += get_stmts_for_gene(gene)
