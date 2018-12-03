ba39bd64eba8ecb8a2685f7287f471cf9326ff86
Ben Gyori <ben.gyori@gmail.com>
2018-12-02 23:59:31 -0500

    Add search capability to model

--- emmaa/model.py
+++ emmaa/model.py
@@ -1,3 +1,6 @@
+from indra.literature import pubmed_client
+
+
 class EmmaaStatement(object):
     def __init__(self, stmt, date, search_terms):
         self.stmt = stmt
@@ -6,8 +9,29 @@ class EmmaaStatement(object):
 
 
 class EmmaaModel(object):
-    def __init__(self):
+    def __init__(self, config):
         self.stmts = []
+        self.search_terms = []
+        self.ndex_network = None
+        self._load_config(config)
 
     def add_statements(self, stmts):
         self.stmts += stmts
+
+    def _load_config(self, config):
+        self.search_terms = config['search_terms']
+        self.ndex_network = config['ndex']['network']
+
+    def search_literature(self, date_limit=None):
+        term_to_pmids = {}
+        for term in self.search_terms:
+            pmids = pubmed_client.get_ids(term, reldate=date_limit)
+            term_to_pmids[term] = pmids
+        pmid_to_terms = {}
+        for term, pmids in term_to_pmids.items():
+            for pmid in pmids:
+                try:
+                    pmid_to_terms[pmid].append(term)
+                except KeyError:
+                    pmid_to_terms[pmid] = [term]
+        return pmid_to_terms
