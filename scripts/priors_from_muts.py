78b864c62de596f809f4c65e7e786bf44b864936
Ben Gyori <ben.gyori@gmail.com>
2018-12-03 00:21:38 -0500

    Get prior statements in script

--- scripts/priors_from_muts.py
+++ scripts/priors_from_muts.py
@@ -1,4 +1,5 @@
-from indra.databases import hgnc_client
+import pickle
+from emmaa.priors.prior_stmts import get_stmts_for_gene_list
 from emmaa.priors.cancer_prior import TcgaCancerPrior
 
 
@@ -8,7 +9,7 @@ def get_terms(ctype):
     nodes = tcp.make_prior(pct_heat_threshold=99)
     cancer_terms = tcp.search_terms_from_nodes(nodes)
     drugs = tcp.find_drugs_for_genes(nodes)
-    return cancer_terms + drugs
+    return cancer_terms, drugs
 
 
 def save_config(ctype, terms):
@@ -18,9 +19,17 @@ def save_config(ctype, terms):
             fh.write(f'- "{term}"\n')
 
 
+def save_prior(stmts):
+    with open(f'models/{ctype}/prior_stmts.pkl', 'wb') as fh:
+        pickle.dump(stmts, fh)
+
+
 if __name__ == '__main__':
     cancer_types = ('paad', 'aml', 'brca', 'luad', 'prad', 'skcm')
 
     for ctype in cancer_types:
-        terms = get_terms(ctype)
+        cancer_terms, drugs = get_terms(ctype)
+        prior_stmts = get_stmts_for_gene_list(cancer_terms)
+        save_prior(prior_stmts)
+        terms = cancer_terms + drugs
         save_config(ctype, terms)
