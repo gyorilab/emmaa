cd00070ab98d8a351aa8f67b8302a1295bdce2ca
Ben Gyori <ben.gyori@gmail.com>
2018-12-04 02:05:11 -0500

    Add pyyaml to setup

--- setup.py
+++ setup.py
@@ -22,5 +22,5 @@ setup(name='emmaa',
         'Programming Language :: Python :: 3'
         ],
       packages=find_packages(),
-      install_requires=['indra', 'boto3']
+      install_requires=['indra', 'boto3', 'pyyaml']
       )
