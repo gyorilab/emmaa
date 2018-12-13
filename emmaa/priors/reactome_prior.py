e9bcffb7587b23c1f06164ca68c0160ad10a48c8
steppi <albert_steppi@hms.harvard.edu>
2018-12-13 14:39:10 -0500

    functions for getting pathways containing a gene and genes contained in a pathway

--- emmaa/priors/reactome_prior.py
+++ emmaa/priors/reactome_prior.py
@@ -1,11 +1,16 @@
 import requests
+import logging
 from functools import lru_cache
+
+
+logger = logging.getLogger('reactome_prior')
+
 
 @lru_cache(10000)
 def rx_id_from_up_id(up_id):
     """Get the Reactome Stable ID for a given Uniprot ID."""
     react_search_url = 'http://www.reactome.org/ContentService/search/query'
-    params = {'query': up_id, 'cluster': 'true', 'species':'Homo sapiens'}
+    params = {'query': up_id, 'cluster': 'true', 'species': 'Homo sapiens'}
     headers = {'Accept': 'application/json'}
     res = requests.get(react_search_url, headers=headers, params=params)
     if not res.status_code == 200:
@@ -13,7 +18,7 @@ def rx_id_from_up_id(up_id):
     json = res.json()
     results = json.get('results')
     if not results:
-        print('No results for %s' % up_id)
+        logger.info(f'No results for {up_id}')
         return None
     stable_ids = []
     for result in results:
@@ -22,7 +27,6 @@ def rx_id_from_up_id(up_id):
             stable_id = entry.get('stId')
             if not stable_id:
                 continue
-            name = entry.get('name')
             stable_ids.append(stable_id)
     return stable_ids
 
@@ -44,3 +48,38 @@ def up_id_from_rx_id(reactome_id):
         return None
     return db_id
 
+
+@lru_cache(1000)
+def get_pathways_containing_gene(reactome_id):
+    """"Get all ids for reactom pathways containing some form of an entity"""
+    react_url = ('http://www.reactome.org/ContentService/data/pathways/low'
+                 f'/entity/{reactome_id}/allForms')
+    params = {'species': 'Homo sapiens'}
+    headers = {'Accept': 'application/json'}
+    res = requests.get(react_url, headers=headers, params=params)
+    if not res.status_code == 200:
+        return None
+    results = res.json()
+    if not results:
+        logger.info(f'No results for {reactome_id}')
+        return None
+    pathway_ids = [pathway['stIdVersion'] for pathway in results]
+    return pathway_ids
+
+
+@lru_cache(1000)
+def get_genes_contained_in_pathway(reactome_id):
+    """Get all genes contained in a given pathway"""
+    react_url = ('http://www.reactome.org/ContentService/data'
+                 f'/participants/{reactome_id}')
+    headers = {'Accept': 'application/json'}
+    res = requests.get(react_url, headers=headers)
+    results = res.json()
+    if not res.status_code == 200:
+        return None
+    if not results:
+        logger.info(f'No results for {reactome_id}')
+    genes = [entity['identifier'] for result in results
+             for entity in result['refEntities']
+             if entity.get('schemaClass') == 'ReferenceGeneProduct']
+    return genes
