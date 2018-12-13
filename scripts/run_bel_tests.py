ec9b8f596b6551088f213bb4f5a1bafd19633dcc
Ben Gyori <ben.gyori@gmail.com>
2018-12-13 17:31:35 -0500

    Simplify BEL testing script

--- scripts/run_bel_tests.py
+++ scripts/run_bel_tests.py
@@ -1,39 +1,30 @@
-import yaml
+import os
+import indra
 from indra.sources import bel
-from indra.assemblers.pysb import PysbAssembler
 from indra.assemblers.english import EnglishAssembler
-from emmaa.model import EmmaaModel
+from emmaa.model import load_model
 from emmaa.model_tests import TestManager, ScopeTestConnector, \
     StatementCheckingTest
 
 
 def get_indirect_stmts():
-    bp = bel.proces_belscript()
+    lcpath = os.path.join(indra.__path__[0], os.pardir, 'data',
+                          'small_corpus.bel')
+    bp = bel.process_belscript(lcpath)
     indirect_stmts = [st for st in bp.statements
-                      if not bp.evidence[0].epistemics('direct')]
+                      if not st.evidence[0].epistemics.get('direct')]
     return indirect_stmts
 
 
-def load_model(ctype):
-    config = yaml.load(open(f'models/{ctype}/config.yaml', 'r'))
-    em = EmmaaModel(ctype, config)
-    em.load_from_s3()
-    return em
-
-
-if __name__ == '__main__'
+if __name__ == '__main__':
     indirect_stmts = get_indirect_stmts()
     tests = [StatementCheckingTest(stmt) for stmt in indirect_stmts]
-    ctypes = ['aml', 'brca', 'luad', 'paad', 'prad', 'skcm']
+    ctypes = ['luad']
     models = []
     for ctype in ctypes:
         # load model from S3
         model = load_model(ctype)
-        stmts = model.get_indra_stmts()
-        pa = PysbAssembler()
-        pa.add_statements(stmts)
-        pysb_model = pa.make_model()
-        models.append(pysb_model)
+        models.append(model)
     # make TestManager
     tm = TestManager(models, tests)
     tm.make_tests(ScopeTestConnector())
