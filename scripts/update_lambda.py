70cd766ea748ca5bd59c4b56c65a24e81386a9dd
pagreene <patrick.anton.greene@gmail.com>
2018-12-14 15:51:49 -0500

    Push file directly to Lambda.

--- scripts/update_lambda.py
+++ scripts/update_lambda.py
@@ -3,17 +3,18 @@ from zipfile import ZipFile
 
 
 def upload_function():
-    """Upload the lambda function by placing it on s3.
+    """Upload the lambda function by pushing a zip :ile to Lambda.
     
     This function pre-supposes you are running from the same directory that
     contains the lambda script, which should be named: `lambda_script.py`.
     """
-    s3 = boto3.client('s3')
+    lamb = boto3.client('lambda')
     with ZipFile('lambda.zip', 'w') as zf:
         zf.write('lambda_script.py')
 
     with open('lambda.zip', 'rb') as zf:
-        s3.put_object(Body=zf, Bucket='emmaa', Key='lambda.zip')
+        lamb.update_function_code(ZipFile=zf.read(),
+                                  FunctionName='emmaa-analysis')
     return
 
 
