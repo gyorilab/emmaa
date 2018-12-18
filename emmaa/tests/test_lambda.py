0b39db6ad56a602e001edcab048aef8cc7ce8d59
pagreene <patrick.anton.greene@gmail.com>
2018-12-18 09:52:52 -0500

    Implement a simple local test of the batch run.

--- emmaa/tests/test_lambda.py
+++ emmaa/tests/test_lambda.py
@@ -2,6 +2,29 @@ import boto3
 import pickle
 from datetime import datetime
 
+from lambda_script import lambda_handler
+
+
+def now_str():
+    return datetime.utcnow().strftime('%Y-%m-%h-%H-%M-%S')
+
+
+def test_handler():
+    """Test the lambda handler locally."""
+    dts = now_str()
+    event = {'Records': [
+        {'s3': {'object': {'key': f'models/test/test_model_{dts}.pkl'}}}
+        ]}
+    context = None
+    res = lambda_handler(event, context)
+    print(res)
+    assert res['statusCode'] == 200, res
+    assert res['body'].startswith('Submitted'), res
+    s3 = boto3.client('s3')
+    s3_res = s3.list_objects(Bucket='emmaa', Prefix='results/test/' + dts[:10])
+    print(s3_res)
+    assert s3_res, s3_res
+
 
 def test_s3_response():
     """Change a file on s3 and check for the correct response."""
