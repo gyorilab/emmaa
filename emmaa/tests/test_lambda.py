4ff83f5a69849ac151cdd1095602e57218f35685
pagreene <patrick.anton.greene@gmail.com>
2018-12-19 10:33:54 -0500

    Finish defining local test.

--- emmaa/tests/test_lambda.py
+++ emmaa/tests/test_lambda.py
@@ -1,24 +1,47 @@
 import boto3
 import pickle
 
-from emmaa.aws_lambda.script import lambda_handler
+from indra.tools.reading.submit_reading_pipeline import wait_for_complete
+
+from emmaa.aws_lambda.script import lambda_handler, QUEUE
 from emmaa.util import make_now_str
+
+RUN_STATI = ['SUBMITTED', 'PENDING', 'RUNNABLE', 'RUNNING']
+DONE_STATI = ['SUCCEEDED', 'FAILED']
+
+
+def __get_jobs(batch):
+    job_ids = {}
+    for status in RUN_STATI + DONE_STATI:
+        resp = batch.list_jobs(jobQueue=QUEUE, jobStatus=status)
+        if 'jobSummaryList' in resp.keys():
+            job_ids[status] = [s['jobId'] for s in resp['jobSummaryList']]
+    return job_ids
 
 
 def test_handler():
     """Test the lambda handler locally."""
     dts = make_now_str()
-    event = {'Records': [
-        {'s3': {'object': {'key': f'models/test/test_model_{dts}.pkl'}}}
-        ]}
+    key = f'models/test/test_model_{dts}.pkl'
+    event = {'Records': [{'s3': {'object': {'key': key}}}]}
     context = None
     res = lambda_handler(event, context)
     print(res)
     assert res['statusCode'] == 200, res
-    assert res['body'].startswith('Submitted'), res
+    assert res['result'] == 'SUCCESS', res
+    assert res['job_id'], res
+    job_id = res['job_id']
+
+    results = {}
+    wait_for_complete(QUEUE, job_list=[{'jobId': job_id}],
+                      result_record=results)
+    print(results)
+    assert job_id in [job_def['jobId'] for job_def in results['succeeded']], \
+        results['failed']
+
     s3 = boto3.client('s3')
     s3_res = s3.list_objects(Bucket='emmaa', Prefix='results/test/' + dts[:10])
-    print(s3_res)
+    print(s3_res.keys())
     assert s3_res, s3_res
 
 
