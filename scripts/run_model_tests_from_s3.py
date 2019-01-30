48a4f82671b621e603e2e10faeedc744d992a009
John Bachman <bachmanjohn@gmail.com>
2019-01-30 14:50:22 -0500

    Add script to run tests from command-line

---
+++ scripts/run_model_tests_from_s3.py
@@ -0,0 +1,15 @@
+import argparse
+from emmaa.model_tests import run_model_tests_from_s3
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+            description='Script to run tests against models, both stored on '
+                        'Amazon S3.')
+    parser.add_argument('-m', '--model', help='Model name', required=True)
+    parser.add_argument('-t', '--test', required=True,
+                        help='Test file name (optional). If not specified, '
+                             'runs all available tests against the model.')
+    args = parser.parse_args()
+
+    run_model_tests_from_s3(args.model, args.test, upload_results=True)
+
