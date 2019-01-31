bfcc6b0306b0eef4e32f48c8b785cc3c66b020ea
John Bachman <bachmanjohn@gmail.com>
2019-01-31 14:18:24 -0500

    Move date funcs back into util

--- emmaa/util.py
+++ emmaa/util.py
@@ -1,7 +1,34 @@
 import os
 import boto3
+from datetime import datetime
 from emmaa.statements import EmmaaStatement
-from emmaa.date import get_date_from_str
+
+
+FORMAT = '%Y-%m-%d-%H-%M-%S'
+
+
+def get_date_from_str(date_str):
+    return datetime.strptime(date_str, FORMAT)
+
+
+def make_date_str(date=None):
+    """Return a date string in a standardized format.
+
+    Parameters
+    ----------
+    date : Optional[datetime.datetime]
+        A date object to get the standardized string for. If not provided,
+        utcnow() is used to construct the date. (Note: using UTC is important
+        because this code may run in multiple contexts).
+
+    Returns
+    -------
+    str
+        The datetime string in a standardized format.
+    """
+    if not date:
+        date = datetime.utcnow()
+    return date.strftime(FORMAT)
 
 
 def find_latest_s3_file(bucket, prefix):
