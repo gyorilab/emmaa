f6cc669abb757b966ffc8712f1cb67d1a0deb3e2
Diana Kolusheva <dianakolusheva@gmail.com>
2019-02-12 17:28:55 -0500

    Updated to take same input and output as aws_reader

--- emmaa/readers/db_client_reader.py
+++ emmaa/readers/db_client_reader.py
@@ -2,8 +2,8 @@ import datetime
 from indra_db.client.statements import get_statements_by_paper
 from emmaa.statements import EmmaaStatement
 
-def get_estatements_by_pmid(pmids):
-    """Return extracted EmmaaStatements from INDRA database given a list of PMIDs.
+def get_estatements_by_pmid_search_terms(pmid_search_terms):
+    """Return extracted EmmaaStatements from INDRA database given a PMID-search term dict.
 
     Parameters
     ----------
@@ -12,16 +12,18 @@ def get_estatements_by_pmid(pmids):
 
     Returns
     -------
-    dict:[str, list[emmaa.model.EmmaaStatement]
+    dict:[str, list[emmaa.model.EmmaaStatement]]
         A dict of PMIDs and the list of EmmaaStatements extracted from INDRA database for given PMIDs.
     """
+    pmids = list(pmid_search_terms.keys())
     date = datetime.datetime.utcnow()
     pmid_stmts = {}
     for pmid in pmids:
-        stmts[pmid] = get_statements_by_paper(pmid, id_type='pmid', 
-                    count=1000, db=None, do_stmt_count=False, preassembled=False)
+        pmid_stmts[pmid] = get_statements_by_paper(pmid, id_type='pmid', 
+                            count=1000, db=None, do_stmt_count=False, preassembled=False)
+    estmsts = []
     for pmid, stmts in pmid_stmts.items():
         for stmt in stmts:
-            estmts.append(es)
             es = EmmaaStatement(stmt, date, pmid_search_terms[pmid])
-    return estmts 
+            estmts.append(es)
+    return estmts
