522809f749556653d174db816483c933b82880f5
Diana Kolusheva <dianakolusheva@gmail.com>
2019-02-13 11:47:09 -0500

    fixed parameters, style

--- emmaa/readers/db_client_reader.py
+++ emmaa/readers/db_client_reader.py
@@ -2,26 +2,28 @@ import datetime
 from indra_db.client.statements import get_statements_by_paper
 from emmaa.statements import EmmaaStatement
 
-def get_estatements_by_pmid_search_terms(pmid_search_terms):
-    """Return extracted EmmaaStatements from INDRA database given a PMID-search term dict.
+def read_db_pmid_search_terms(pmid_search_terms):
+    """Return extracted EmmaaStatements from INDRA database given a 
+    PMID-search term dict.
 
     Parameters
     ----------
-    pmids : list[str]
-        A list of PMIDs to check with a database.
+    pmid_search_terms : dict
+        A dict representing a set of PMIDs pointing to search terms that
+        produced them.
 
     Returns
     -------
-    dict:[str, list[emmaa.model.EmmaaStatement]]
-        A dict of PMIDs and the list of EmmaaStatements extracted from INDRA database for given PMIDs.
+    list[:py:class:`emmaa.model.EmmaaStatement`]
+        A list of EmmaaStatements extracted from the given PMIDs.
     """
     pmids = list(pmid_search_terms.keys())
     date = datetime.datetime.utcnow()
     pmid_stmts = {}
     for pmid in pmids:
-        pmid_stmts[pmid] = get_statements_by_paper(pmid, id_type='pmid', 
-                            count=1000, db=None, do_stmt_count=False, preassembled=False)
-    estmsts = []
+        pmid_stmts[pmid] = get_statements_by_paper(
+            pmid, id_type='pmid', preassembled=False)
+    estmts = []
     for pmid, stmts in pmid_stmts.items():
         for stmt in stmts:
             es = EmmaaStatement(stmt, date, pmid_search_terms[pmid])
