a3be0223cc065d3057992c01ea17af4fdb3ba0b8
Diana Kolusheva <dianakolusheva@gmail.com>
2019-02-27 11:28:02 -0500

    Removed some old code

--- emmaa/model_tests.py
+++ emmaa/model_tests.py
@@ -76,11 +76,6 @@ class TestManager(object):
             logger.info(f'Created {len(model.applicable_tests)} tests for '
                         f'{model.model.name} model.')
 
-    # def run_tests(self):
-    #     """Run tests for a list of model-test pairs"""
-    #     for (model, test) in self.pairs_to_test:
-    #         self.test_results.append(test.check(model))
-
     def run_tests(self):
         """Run tests for a list of model-test pairs"""
         for model in self.models:
@@ -136,16 +131,6 @@ class EmmaaTest(object):
 class StatementCheckingTest(EmmaaTest):
     """Represent an EMMAA test condition that checks a PySB-assembled model
     against an INDRA Statement."""
-    # def __init__(self, stmt):
-    #     self.stmt = stmt
-
-    # def check(self, model):
-    #     """Use a model checker to check if a given model satisfies the test."""
-    #     pysb_model = model.assemble_pysb()
-    #     mc = ModelChecker(pysb_model, [self.stmt])
-    #     res = mc.check_statement(self.stmt)
-    #     return res
-
     def __init__(self, stmt):
         self.stmt = stmt
         # TODO
