7bd2833f0d700f2c582ae2f3f2a564a9bf5e798a
dianakolusheva <dianakolusheva@gmail.com>
2019-03-14 16:14:21 -0400

    Reversed unnecessary change

--- scripts/get_model_test_stats.py
+++ scripts/get_model_test_stats.py
@@ -7,7 +7,7 @@ from emmaa.util import find_latest_s3_file, get_s3_client
 logger = logging.getLogger(__name__)
 
 
-def load_latest_test_results_from_s3(model_name):
+def load_test_results_from_s3(model_name):
     base_key = f'results/{model_name}'
     latest_result_key = find_latest_s3_file('emmaa', f'{base_key}/results_',
                                             extension='.json')
@@ -20,7 +20,7 @@ def load_latest_test_results_from_s3(model_name):
 
 def show_statistics(model_name):
     try:
-        test_results = load_latest_test_results_from_s3(model_name)
+        test_results = load_test_results_from_s3(model_name)
     except IndexError:
         print('No test results for ' + model_name + ' model available.')
     else:
