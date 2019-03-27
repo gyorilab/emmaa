59855ca41c8a32afe3121d6d73526d9ce95ca77d
kkaris <karis.klas@gmail.com>
2019-03-27 14:09:09 -0400

    Add init. Add test_api.py

---
+++ rest_api/test_api.py
@@ -0,0 +1,36 @@
+import unittest
+import json
+import sys
+
+from .api import app
+
+
+class EmmaaApiTest(unittest.TestCase):
+
+    def setUp(self):
+        app.testing = True
+        self.app = app.test_client()
+
+    def test_query_endpoint(self):
+        """Test if we can submit a simple model query
+
+        NOTE: only tests that the query was recieved by the API and that it
+        was properly formatted.
+        """
+
+        test_query = {'user': {'email': 'joshua@emmaa.com',
+                               'name': 'joshua',
+                               'slack_id': '123456abcdef'},
+                      'query': {'models': ['aml', 'luad'],
+                                'objectSelection': 'ERK',
+                                'subjectSelection': 'BRAF',
+                                'typeSelection': 'activation'},
+                      'register': 'false',
+                      'test': 'true'}
+
+        data = json.dumps(test_query)
+
+        resp = self.app.post('/query/submit', data=data)
+
+        assert resp.status_code == 200
+
