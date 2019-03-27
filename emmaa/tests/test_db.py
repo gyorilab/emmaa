f60b1e644f80b2e59dfc07f1fd22b9dd951c8bfa
pagreene <patrick.anton.greene@gmail.com>
2019-03-27 18:29:02 -0400

    Add test of put_models.

--- emmaa/tests/test_db.py
+++ emmaa/tests/test_db.py
@@ -1,4 +1,6 @@
-from emmaa.db import get_db, Query
+import random
+
+from emmaa.db import get_db, Query, Result
 
 
 def _test_db():
@@ -37,3 +39,18 @@ def test_get_queries():
         db.put_queries(query, ['aml', 'luad'])
     queries = db.get_queries('aml')
     assert len(queries) == 2, len(queries)
+
+
+def test_put_models():
+    db = _test_db()
+    test_query = {'objectSelection': 'ERK',
+                  'subjectSelection': 'BRAF',
+                  'typeSelection': 'activation'}
+    db.put_queries(test_query, ['aml', 'luad'])
+    queries = db.get_queries('aml')
+    results = [(query, random.choice(['This is fine.', 'This is not ok.']))
+               for query in queries]
+    db.put_results('aml', results)
+    with db.get_session() as sess:
+        db_results = sess.query(Result).all()
+    assert len(db_results) == len(results)
