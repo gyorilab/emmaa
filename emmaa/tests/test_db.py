54ed923562436ea19abcdef1147623a214a95116
pagreene <patrick.anton.greene@gmail.com>
2019-03-27 17:22:37 -0400

    Add test for query submission.

--- emmaa/tests/test_db.py
+++ emmaa/tests/test_db.py
@@ -1,4 +1,4 @@
-from emmaa.db import get_db
+from emmaa.db import get_db, Query
 
 
 def _test_db():
@@ -12,3 +12,15 @@ def test_instantiation():
     db = _test_db()
     assert db
     return
+
+
+def test_put_queries():
+    db = _test_db()
+    test_query = {'objectSelection': 'ERK',
+                  'subjectSelection': 'BRAF',
+                  'typeSelection': 'activation'}
+    db.put_queries(test_query, ['aml', 'luad'])
+    with db.get_session() as sess:
+        queries = sess.query(Query).all()
+    assert len(queries) == 2, len(queries)
+
