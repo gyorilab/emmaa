8fcd62d596a851b2dedf767ebc3bfe39ff5f6910
pagreene <patrick.anton.greene@gmail.com>
2019-03-27 15:52:30 -0400

    Create general function for creating test db.

--- emmaa/tests/test_db.py
+++ emmaa/tests/test_db.py
@@ -1,7 +1,14 @@
 from emmaa.db import get_db
 
 
-def test_instantiation():
-    db = get_db('local test')
+def _test_db():
+    db = get_db('AWS test')
+    db.drop_tables(force=True)
     db.create_tables()
+    return db
+
+
+def test_instantiation():
+    db = _test_db()
+    assert db
     return
