3d3c3e8ab0b509d1b0d71fd770a79d2b8b94a5ba
pagreene <patrick.anton.greene@gmail.com>
2019-03-28 14:23:39 -0400

    Move js from query html into separate file.

---
+++ dashboard/queryFunctions.js
@@ -0,0 +1,124 @@
+// EMMAA rest API
+var EMMAA_API = './query/submit'
+
+$(document).ready(function() {
+  fillNamespaceOptions()
+})
+
+function postQuery(queryContainer) {
+  console.log('function postQuery(queryContainer)')
+  // Get user info
+  userInfo = {
+    name: 'joshua',
+    slack_id: '123456abcdef',
+    email: 'joshua@emmaa.com'
+  }
+
+  // Collect model query
+  let querySel = collectQuery(queryContainer)
+
+  // Check if user wants to register query
+  let reg = document.getElementById('register-query').checked
+
+  let ajax_response = submitQuery({
+    user: userInfo,
+    query: querySel,
+    register: reg
+  }, null)
+
+  console.log("ajax response from submission: ");
+  console.log(ajax_response);
+}
+
+function collectQuery(queryContainer) {
+  console.log('function collectQuery(queryContainer)')
+  let dropdownSelections = queryContainer.getElementsByClassName('custom-select')
+
+  query = {};
+  checkboxes = []
+
+  // Get checked models
+  for (box of document.getElementById('model-select').getElementsByTagName('input')) {
+    console.log(box.name)
+    if (!box.disabled && box.checked) {
+      checkboxes.push(box.name)
+    }
+  }
+  query['models'] = checkboxes;
+
+  //  Collect dropdown selections
+  for (selection of dropdownSelections) {
+    selId = selection.id;
+    // Use the IDs of the select tags as keys in the query json
+    query[selId] = selection.options[selection.selectedIndex].value;
+  }
+
+  // Collect subject/object from forms
+  query['subjectSelection'] = document.getElementById('subjectInput').value;
+  query['objectSelection'] = document.getElementById('objectInput').value;
+
+  return query;
+}
+
+function submitQuery(queryDict, test) {
+  console.log('function submitQuery(queryDict)')
+  console.log('Submitting data to query DB')
+  console.log(queryDict)
+
+  if (test) queryDict['test'] = true;
+
+  // submit POST to emmaa user db
+  let response = $.ajax({
+    url: EMMAA_API,
+    type: 'POST',
+    dataType: 'json',
+    contentType: 'application/json',
+    data: JSON.stringify(queryDict),
+    complete: function(xhr, statusText) {
+      console.log('responseJSON')
+      console.log(xhr.responseJSON)
+      console.log(statusText)
+      switch (xhr.status) {
+        case 200:
+          console.log('200 response')
+          populateQueryResults(xhr.responseJSON)
+          break;
+        case 400:
+          console.log('400 response')
+          break;
+        case 401:
+          console.log('401 response')
+          break;
+        case 404:
+          console.log('404 response')
+          break;
+        default:
+          console.log('Unhandled server response: ' + xhr.status)
+      }
+    }
+  })
+  return response;
+}
+
+function populateQueryResults(json) {
+  console.log('function populateQueryResults(json)')
+  console.log(json)
+  let qrTable = document.getElementById('queryResults');
+  clearTable(qrTable)
+  for (model in json.result) {
+    qrTable.appendChild(addToRow([model, json.result[model]]));
+  }
+}
+
+function fillNamespaceOptions() {
+  for (nsDD of document.getElementsByClassName('namespace-dropdown')) {
+    console.log(nsDD)
+    for (n_v of NAMESPACE_OPTIONS) {
+      let optionTag = document.createElement('option');
+      optionTag.value = n_v[1];
+      optionTag.textContent = n_v[0];
+      console.log(optionTag.textContent)
+      nsDD.appendChild(optionTag);
+    }
+  }
+}
