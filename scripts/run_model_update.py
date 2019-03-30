9a1e7a644691b5d040f86b1d32dd83596e0639c9
Ben Gyori <ben.gyori@gmail.com>
2019-03-30 17:08:56 -0400

    Add RAS Machine model to update script

--- scripts/run_model_update.py
+++ scripts/run_model_update.py
@@ -2,7 +2,8 @@ from emmaa.model import EmmaaModel
 
 
 if __name__ == '__main__':
-    cancer_types = ('aml', 'brca', 'luad', 'paad', 'prad', 'skcm')
+    cancer_types = ('aml', 'brca', 'luad', 'paad', 'prad', 'skcm',
+                    'rasmachine')
 
     for ctype in cancer_types:
         em = EmmaaModel.load_from_s3(ctype)
