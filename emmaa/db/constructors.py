fbda1955abc51d44f6f7e55b216d57c3441879af
Ben Gyori <ben.gyori@gmail.com>
2019-04-02 20:48:54 -0400

    Fix handling test DB before indexing defaults

--- emmaa/db/constructors.py
+++ emmaa/db/constructors.py
@@ -10,12 +10,13 @@ logger = logging.getLogger(__name__)
 
 def get_db(name):
     """Get a db instance based on its name in the config or env."""
-    defaults = get_databases()
-    db_name = defaults[name]
+    if name == 'test' and 'EMMAA_TEST_DB' in os.environ:
+        db_name = os.environ['EMMAA_TEST_DB']
+    else:
+        defaults = get_databases()
+        db_name = defaults[name]
     m = re.match('(\w+)://.*?/([\w.]+)', db_name)
     if m is None:
         logger.error("Poorly formed db name: %s" % db_name)
         return
-    if name == 'test' and 'EMMAA_TEST_DB' in os.environ:
-        dm_name = os.environ['EMMAA_TEST_DB']
     return EmmaaDatabaseManager(db_name, label=name)
