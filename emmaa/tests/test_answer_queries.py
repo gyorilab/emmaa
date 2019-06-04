269e7a9443fe228fd5e6c8852f0d79526f28733b
dianakolusheva <dianakolusheva@gmail.com>
2019-06-04 11:50:44 -0400

    Add more tests for QueryManager

--- emmaa/tests/test_answer_queries.py
+++ emmaa/tests/test_answer_queries.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from nose.plugins.attrib import attr
-from emmaa.answer_queries import QueryManager, get_registered_queries, \
-    format_results, load_model_manager_from_s3
+from emmaa.answer_queries import QueryManager, format_results, \
+    load_model_manager_from_s3, _is_diff
 from emmaa.queries import Query
 from emmaa.model_tests import ModelManager
 from emmaa.db import get_db
@@ -10,7 +10,6 @@ from indra.statements.agent import Agent
 
 
 # Tell nose to not run tests in the imported modules
-get_registered_queries.__test__ = False
 format_results.__test__ = False
 load_model_manager_from_s3.__test__ = False
 ModelManager.__test__ = False
@@ -19,6 +18,7 @@ Agent.__test__ = False
 get_db.__test__ = False
 Query.__test__ = False
 QueryManager.__test__ = False
+_is_diff.__test__ = False
 
 
 test_query = {'type': 'path_property', 'path': {
@@ -30,6 +30,10 @@ test_response = {3801854542: [
      'https://db.indra.bio/statements/from_agents?subject=1097@HGNC&object=6840@HGNC&type=Activation&format=html'),
     ('Active MAP2K1 activates MAPK1.',
      'https://db.indra.bio/statements/from_agents?subject=6840@HGNC&object=6871@HGNC&type=Activation&format=html')]}
+processed_link = '<a href="https://db.indra.bio/statements/from_agents?subject=1097@HGNC&object=6840@HGNC&type=Activation&format=html" target="_blank" class="status-link">BRAF activates MAP2K1.</a>'
+query_not_appl = {2413475507: [
+    ('Query is not applicable for this model',
+     'https://emmaa.readthedocs.io/en/latest/dashboard/response_codes.html')]}
 
 
 def test_load_model_manager_from_s3():
@@ -67,10 +71,53 @@ def test_answer_get_registered_queries():
     qm._recreate_db()
     qm.db.put_queries('tester@test.com', processed_query, ['test'], subscribe=True)
     qm.answer_registered_queries('test')
-    results = get_registered_queries('tester@test.com', db_name='test')
+    results = qm.get_registered_queries('tester@test.com')
     assert len(results) == 1
     assert results[0]['model'] == 'test'
     assert results[0]['query'] == processed_query
     assert isinstance(results[0]['response'], str)
     assert 'BRAF activates MAP2K1.' in results[0]['response'], results[0]['response']
     assert isinstance(results[0]['date'], str)
+
+
+def test_is_diff():
+    assert not _is_diff(query_not_appl, query_not_appl)
+    assert _is_diff(test_response, query_not_appl)
+
+
+@attr('nonpublic')
+def test_report_one_query():
+    qm = QueryManager(db_name='test')
+    str_msg = qm.make_str_report_one_query(
+        'test', processed_query, test_response, query_not_appl)
+    assert str_msg
+    assert 'A new result to query' in str_msg
+    assert 'Query is not applicable for this model' in str_msg
+    assert 'BRAF activates MAP2K1.' in str_msg
+    html_msg = qm.make_html_one_query_report(
+        'test', processed_query, test_response, query_not_appl)
+    assert html_msg
+    assert 'A new result to query' in html_msg
+    assert 'Query is not applicable for this model' in html_msg
+    assert processed_link in html_msg
+
+
+@attr('nonpublic')
+def test_report_files():
+    qm = QueryManager(db_name='test')
+    qm._recreate_db()
+    qm.db.put_queries('tester@test.com', processed_query, ['test'], subscribe=True)
+    qm.db.put_results('test', [(processed_query, query_not_appl)])
+    qm.make_str_report_per_user('tester@test.com', filename='test_query_delta.txt')
+    with open('test_query_delta.txt', 'r') as f:
+        msg = f.read()
+    assert msg
+    assert 'This is the first result to query' in msg, msg
+    assert 'Query is not applicable for this model' in msg
+    qm.db.put_results('test', [(processed_query, test_response)])
+    qm.make_str_report_per_user('tester@test.com', filename='test_query_delta.txt')
+    with open('test_query_delta.txt', 'r') as f:
+        msg = f.read()
+    assert msg
+    assert 'A new result to query' in msg
+    assert 'BRAF activates MAP2K1.' in msg
