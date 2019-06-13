790bac9d48411dbea3fea52465462d22b74ecff2
dianakolusheva <dianakolusheva@gmail.com>
2019-06-13 14:40:55 -0400

    Make hash and string repr of queries

--- emmaa/tests/test_queries.py
+++ emmaa/tests/test_queries.py
@@ -40,3 +40,13 @@ def test_path_property_to_json():
     deserialize_query = Query._from_json(json)
     json2 = deserialize_query.to_json()
     assert json == json2, {'json': json, 'json2': json2}
+
+
+def test_stringify_path_property():
+    stmt = Phosphorylation(enz=Agent('EGFR', db_refs={'HGNC': '3236'}),
+                           sub=Agent('ERK', db_refs={'FPLX': 'ERK'}))
+    entity_constraints = {'exclude': [Agent('PI3K', db_refs={'FPLX': 'PI3K'})]}
+    relationship_contraints = {'exclude': ['IncreaseAmount', 'DecreaseAmount']}
+    query = PathProperty(stmt, entity_constraints, relationship_contraints)
+    query_str = str(query)
+    assert query_str == 'PathPropertyQuery(stmt=Phosphorylation(EGFR(), ERK()). Exclude entities: PI3K(). Exclude relations: IncreaseAmount, DecreaseAmount.'
