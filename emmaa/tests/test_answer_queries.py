c103757a68832a7736b95d5c690a11ab58ecab31
dianakolusheva <dianakolusheva@gmail.com>
2019-06-14 10:28:03 -0400

    Make tests pass

--- emmaa/tests/test_answer_queries.py
+++ emmaa/tests/test_answer_queries.py
@@ -22,9 +22,12 @@ _is_diff.__test__ = False
 
 
 test_query = {'type': 'path_property', 'path': {
-    'type': 'Activation', 'subj': {'type': 'Agent', 'name': 'BRAF'},
-    'obj': {'type': 'Agent', 'name': 'MAPK1'}}}
-processed_query = Query._from_json(test_query)
+              'type': 'Activation', 'subj': {'type': 'Agent', 'name': 'BRAF'},
+              'obj': {'type': 'Agent', 'name': 'MAPK1'}}}
+simple_query = {'typeSelection': 'Activation',
+                'subjectSelection': 'BRAF',
+                'objectSelection': 'MAPK1'}
+query_object = Query._from_json(test_query)
 test_response = {3801854542: [
     ('BRAF activates MAP2K1.',
      'https://db.indra.bio/statements/from_agents?subject=1097@HGNC&object=6840@HGNC&type=Activation&format=html'),
@@ -42,11 +45,11 @@ def test_load_model_manager_from_s3():
 
 
 def test_format_results():
-    results = [('test', test_query, test_response, datetime.now())]
+    results = [('test', query_object, test_response, datetime.now())]
     formatted_results = format_results(results)
     assert len(formatted_results) == 1
     assert formatted_results[0]['model'] == 'test'
-    assert formatted_results[0]['query'] == test_query
+    assert formatted_results[0]['query'] == simple_query
     assert isinstance(formatted_results[0]['response'], str)
     assert isinstance(formatted_results[0]['date'], str)
 
@@ -55,11 +58,11 @@ def test_format_results():
 def test_answer_immediate_query():
     qm = QueryManager(db_name='test')
     qm._recreate_db()
-    results = qm.answer_immediate_query('tester@test.com', processed_query,
+    results = qm.answer_immediate_query('tester@test.com', query_object,
                                         ['test'], subscribe=False)
     assert len(results) == 1
     assert results[0]['model'] == 'test'
-    assert results[0]['query'] == processed_query, (results[0]['query'], processed_query)
+    assert results[0]['query'] == simple_query
     assert isinstance(results[0]['response'], str)
     assert 'BRAF activates MAP2K1.' in results[0]['response'], results[0]['response']
     assert isinstance(results[0]['date'], str)
@@ -69,12 +72,12 @@ def test_answer_immediate_query():
 def test_answer_get_registered_queries():
     qm = QueryManager(db_name='test')
     qm._recreate_db()
-    qm.db.put_queries('tester@test.com', processed_query, ['test'], subscribe=True)
+    qm.db.put_queries('tester@test.com', query_object, ['test'], subscribe=True)
     qm.answer_registered_queries('test')
     results = qm.get_registered_queries('tester@test.com')
     assert len(results) == 1
     assert results[0]['model'] == 'test'
-    assert results[0]['query'] == processed_query
+    assert results[0]['query'] == simple_query
     assert isinstance(results[0]['response'], str)
     assert 'BRAF activates MAP2K1.' in results[0]['response'], results[0]['response']
     assert isinstance(results[0]['date'], str)
@@ -89,13 +92,13 @@ def test_is_diff():
 def test_report_one_query():
     qm = QueryManager(db_name='test')
     str_msg = qm.make_str_report_one_query(
-        'test', processed_query, test_response, query_not_appl)
+        'test', query_object, test_response, query_not_appl)
     assert str_msg
     assert 'A new result to query' in str_msg
     assert 'Query is not applicable for this model' in str_msg
     assert 'BRAF activates MAP2K1.' in str_msg
     html_msg = qm.make_html_one_query_report(
-        'test', processed_query, test_response, query_not_appl)
+        'test', query_object, test_response, query_not_appl)
     assert html_msg
     assert 'A new result to query' in html_msg
     assert 'Query is not applicable for this model' in html_msg
@@ -106,15 +109,15 @@ def test_report_one_query():
 def test_report_files():
     qm = QueryManager(db_name='test')
     qm._recreate_db()
-    qm.db.put_queries('tester@test.com', processed_query, ['test'], subscribe=True)
-    qm.db.put_results('test', [(processed_query, query_not_appl)])
+    qm.db.put_queries('tester@test.com', query_object, ['test'], subscribe=True)
+    qm.db.put_results('test', [(query_object, query_not_appl)])
     qm.make_str_report_per_user('tester@test.com', filename='test_query_delta.txt')
     with open('test_query_delta.txt', 'r') as f:
         msg = f.read()
     assert msg
     assert 'This is the first result to query' in msg, msg
     assert 'Query is not applicable for this model' in msg
-    qm.db.put_results('test', [(processed_query, test_response)])
+    qm.db.put_results('test', [(query_object, test_response)])
     qm.make_str_report_per_user('tester@test.com', filename='test_query_delta.txt')
     with open('test_query_delta.txt', 'r') as f:
         msg = f.read()
