1bf65104139fa9ca54e812659c45d25be68f810a
dianakolusheva <dianakolusheva@gmail.com>
2019-06-18 11:07:33 -0400

    Add test configs to run_one_test

--- emmaa/model_tests.py
+++ emmaa/model_tests.py
@@ -87,6 +87,8 @@ class ModelManager(object):
         self.model_checker.statements = []
         self.model_checker.add_statements([test.stmt])
         self.get_im()
+        if not test.configs:
+            test.configs = self.model.test_config.get('statement_checking')
         return test.check(self.model_checker, self.pysb_model)
 
     def run_tests(self):
