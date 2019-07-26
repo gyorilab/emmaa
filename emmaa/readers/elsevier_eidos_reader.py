d52ec790c069d80022cfb9dfdd4b8eaf7bb12a6c
dianakolusheva <dianakolusheva@gmail.com>
2019-07-26 11:31:23 -0400

    Separate different functions and handle errors

--- emmaa/readers/elsevier_eidos_reader.py
+++ emmaa/readers/elsevier_eidos_reader.py
@@ -12,7 +12,8 @@ logger = logging.getLogger(__name__)
 def read_elsevier_eidos_search_terms(ids_to_terms):
     piis = list(ids_to_terms.keys())
     date = datetime.datetime.utcnow()
-    pii_stmts = read_piis(piis)
+    texts = read_piis(piis)
+    pii_stmts = process_texts(texts)
     estmts = []
     for pii, stmts in pii_stmts.items():
         for stmt in stmts:
@@ -22,17 +23,32 @@ def read_elsevier_eidos_search_terms(ids_to_terms):
 
 
 def read_piis(piis):
-    eidos_url = os.environ.get('EIDOS_URL')
-    logger.info('Reading with Eidos URL: %s' % eidos_url)
-    pii_stmts = {}
+    texts = {}
     for pii in piis:
-        pii_stmts[pii] = []
         xml = elsevier_client.download_article(pii, id_type='pii')
         if not xml:
             logger.info('Could not get article content for %s' % pii)
             continue
         txt = elsevier_client.extract_text(xml)
-        ep = eidos.process_text(txt, webservice=eidos_url)
-        if ep:
-            pii_stmts[pii] = ep.statements
+        if not txt:
+            logger.info('Could not extract article text for %s' % pii)
+            continue
+        texts[pii] = txt
+    logger.info('Got text back for %d articles.' % len(texts))
+    return texts
+
+
+def process_texts(texts):
+    eidos_url = os.environ.get('EIDOS_URL')
+    logger.info('Reading with Eidos URL: %s' % eidos_url)
+    pii_stmts = {}
+    for pii, txt in texts.items():
+        logger.info('Reading the article with %s pii.' % pii)
+        try:
+            ep = eidos.process_text(txt, webservice=eidos_url)
+            if ep:
+                pii_stmts[pii] = ep.statements
+        except Exception as e:
+            logger.info('Could not read the text because of %s' % str(e))
+            continue
     return pii_stmts
