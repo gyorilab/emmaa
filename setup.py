4f27b6826c757a3252f0eb848f7d6ba0fcbb1d8b
Ben Gyori <ben.gyori@gmail.com>
2019-08-01 20:10:42 -0400

    Bump version to 1.3.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 
 setup(name='emmaa',
-      version='1.2.2',
+      version='1.3.0',
       description='Ecosystem of Machine-maintained Models with ' + \
                   'Automated Analysis',
       long_description=long_description,
