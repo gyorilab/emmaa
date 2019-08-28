f75a47b24c774dfade6c2e7b11b3143666737e7e
dianakolusheva <dianakolusheva@gmail.com>
2019-08-28 13:57:30 -0400

    Enable showing MC type in queries

--- emmaa/answer_queries.py
+++ emmaa/answer_queries.py
@@ -287,8 +287,8 @@ def format_results(results):
         formatted_result['model'] = result[0]
         query = result[1]
         formatted_result['query'] = _make_query_simple_dict(query)
-        # mc_type = result[2]
-        # formatted_result['mc_type'] = mc_type
+        mc_type = result[2]
+        formatted_result['mc_type'] = mc_type
         response_json = result[3]
         response = _process_result_to_html(response_json)
         formatted_result['response'] = response
