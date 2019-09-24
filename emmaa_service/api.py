093e89d55e426e790b1d3540d6fab7bd63587f4c
kkaris <karis.klas@gmail.com>
2019-09-24 09:47:10 -0400

    Add Test as current model for header

--- emmaa_service/api.py
+++ emmaa_service/api.py
@@ -299,7 +299,7 @@ def get_model_dashboard(model):
                            stmts_counts=top_stmts_counts,
                            added_stmts=added_stmts,
                            model_info_contents=model_info_contents,
-                           model_types=current_model_types,
+                           model_types=["Test", *current_model_types],
                            new_applied_tests=_new_applied_tests(model_stats))
 
 
