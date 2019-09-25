33fceaa46b19d94eb5ae57704ebcb3decf122962
kkaris <karis.klas@gmail.com>
2019-09-25 10:13:26 -0400

    Add flask_jwt_extended to setup.py

--- setup.py
+++ setup.py
@@ -25,6 +25,6 @@ setup(name='emmaa',
       packages=find_packages(),
       install_requires=['indra', 'boto3', 'jsonpickle', 'kappy==4.0.0rc1',
                         'pygraphviz', 'fnvhash', 'sqlalchemy', 'inflection',
-                        'pybel'],
+                        'pybel', 'flask_jwt_extended'],
       extras_require={'test': ['nose', 'coverage', 'python-coveralls']}
       )
