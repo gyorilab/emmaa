c3438a56ab24d835fc4380b95507a135e338a832
dianakolusheva <dianakolusheva@gmail.com>
2019-12-13 12:02:36 -0500

    Add and fix api functions tests

--- emmaa/tests/test_api_functions.py
+++ emmaa/tests/test_api_functions.py
@@ -1,7 +1,8 @@
 import re
 
-from emmaa.util import RE_DATEFORMAT
-from emmaa_service.api import get_model_stats, model_last_updated
+from emmaa.util import RE_DATETIMEFORMAT, RE_DATEFORMAT
+from emmaa_service.api import get_model_stats, model_last_updated, \
+    latest_stats_date
 
 MODEL = 'aml'
 
@@ -9,9 +10,16 @@ MODEL = 'aml'
 def test_last_updated():
     key_str = model_last_updated(MODEL)
     assert key_str
+    assert re.search(RE_DATETIMEFORMAT, key_str).group()
+
+
+def test_latest_stats():
+    key_str = latest_stats_date(MODEL)
+    assert key_str
     assert re.search(RE_DATEFORMAT, key_str).group()
 
 
 def test_get_model_statistics():
-    model_json = get_model_stats(MODEL)
+    date = latest_stats_date(MODEL)
+    model_json = get_model_stats(MODEL, date)
     assert isinstance(model_json, dict)
