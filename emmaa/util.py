9ae34b55e3ecebf0715add7b4883d2a62115f5d7
kkaris <karis.klas@gmail.com>
2019-12-12 19:35:05 -0500

    Add wrapper returning keys for latest emails

--- emmaa/util.py
+++ emmaa/util.py
@@ -133,6 +133,29 @@ def find_number_of_files_on_s3(bucket, prefix, extension=None):
     return len(files)
 
 
+def find_latest_emails(email_type, past_hours=24):
+    """Return a list of keys of the latest emails delivered to s3
+
+    Parameters
+    ----------
+    email_type : str
+        The email type to look for, e.g. 'feedback' if listing bounce and
+        complaint emails sent to the ReturnPath address.
+    past_hours : int
+        The time interval in number of hours to look backwards for listing
+        emails.
+
+    Returns
+    -------
+    list[Keys]
+        A list of keys to the emails of the specified type.
+    """
+    email_list = sort_s3_files_by_last_mod(email_bucket, email_type,
+                                           past_hours, unsigned=False)
+    ignore = 'AMAZON_SES_SETUP_NOTIFICATION'
+    return [s for s in email_list if ignore not in s]
+
+
 def does_exist(bucket, prefix, extension=None):
     """Check if the file with exact key or starting with prefix and/or with
     extension exist in a bucket.
