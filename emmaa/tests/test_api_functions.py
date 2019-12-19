fbf5ac1cf1eea0e2dc708b9a34a06fd9b95775cb
dianakolusheva <dianakolusheva@gmail.com>
2019-12-19 12:24:00 -0500

    Add tests for api functions

--- emmaa/tests/test_api_functions.py
+++ emmaa/tests/test_api_functions.py
@@ -1,25 +1,29 @@
-import re
-
-from emmaa.util import RE_DATETIMEFORMAT, RE_DATEFORMAT
-from emmaa_service.api import get_model_stats, model_last_updated, \
-    latest_stats_date
-
-MODEL = 'aml'
+from emmaa_service.api import _get_model_meta_data, get_model_config,\
+    _make_query
+from emmaa.queries import PathProperty
 
 
-def test_last_updated():
-    key_str = model_last_updated(MODEL)
-    assert key_str
-    assert re.search(RE_DATETIMEFORMAT, key_str).group()
+def test_get_metadata():
+    metadata = _get_model_meta_data()
+    assert len(metadata) == 11, len(metadata)
+    assert len(metadata[0]) == 3
+    assert isinstance(metadata[0][0], str)
+    assert isinstance(metadata[0][1], dict)
+    assert isinstance(metadata[0][2], str)
 
 
-def test_latest_stats():
-    key_str = latest_stats_date(MODEL)
-    assert key_str
-    assert re.search(RE_DATEFORMAT, key_str).group()
+def test_get_model_config():
+    config = get_model_config('aml')
+    assert config
+    assert isinstance(config, dict)
+    # can't get if there's no human readable name
+    config = get_model_config('test')
+    assert not config
 
 
-def test_get_model_statistics():
-    date = latest_stats_date(MODEL)
-    model_json = get_model_stats(MODEL, date)
-    assert isinstance(model_json, dict)
+def test_make_query():
+    query = _make_query({
+        'typeSelection': 'Activation',
+        'subjectSelection': 'BRAF',
+        'objectSelection': 'MAPK1'})
+    assert isinstance(query, PathProperty)
