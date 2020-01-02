de1f1daff52d88108d0cf8352a63dc7f1fdf45c6
dianakolusheva <dianakolusheva@gmail.com>
2020-01-02 12:21:17 -0500

    Add lambda function for generating stats

--- scripts/run_model_stats_from_s3.py
+++ scripts/run_model_stats_from_s3.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     parser.add_argument('-m', '--model', help='Model name', required=True)
     parser.add_argument('-t', '--tests', required=True,
                         help='Test file name (optional). Default is '
-                        'large_corpus_tests.pkl')
+                        'large_corpus_tests')
     args = parser.parse_args()
 
     generate_model_stats_on_s3(args.model, args.tests, upload_stats=True)
