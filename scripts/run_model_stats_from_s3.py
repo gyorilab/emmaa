00035708c699e41fc8a790343857af8a671be0f3
dianakolusheva <dianakolusheva@gmail.com>
2020-01-02 12:09:45 -0500

    Add tests arguments to stats script

--- scripts/run_model_stats_from_s3.py
+++ scripts/run_model_stats_from_s3.py
@@ -7,6 +7,9 @@ if __name__ == '__main__':
             description='Script to generate statistic for the latest round of'
                         'tests and save to Amazon S3.')
     parser.add_argument('-m', '--model', help='Model name', required=True)
+    parser.add_argument('-t', '--tests', required=True,
+                        help='Test file name (optional). Default is '
+                        'large_corpus_tests.pkl')
     args = parser.parse_args()
 
-    generate_model_stats_on_s3(args.model, upload_stats=True)
+    generate_model_stats_on_s3(args.model, args.tests, upload_stats=True)
