37a354a357eba6c2ca0020d414ad090151739d21
dianakolusheva <dianakolusheva@gmail.com>
2020-01-27 17:58:05 -0500

    Update test api functions

--- emmaa/tests/test_api_functions.py
+++ emmaa/tests/test_api_functions.py
@@ -1,10 +1,17 @@
 from emmaa_service.api import _make_query
-from emmaa.queries import PathProperty
+from emmaa.queries import PathProperty, DynamicProperty
 
 
 def test_make_query():
-    query = _make_query({
+    query, tab = _make_query({
         'typeSelection': 'Activation',
         'subjectSelection': 'BRAF',
         'objectSelection': 'MAPK1'})
     assert isinstance(query, PathProperty)
+    assert tab == 'static'
+    query, tab = _make_query({
+        'agentSelection': 'phosphorylated MAP2K1',
+        'valueSelection': 'low',
+        'patternSelection': 'always_value'})    
+    assert isinstance(query, DynamicProperty)
+    assert tab == 'dynamic'
