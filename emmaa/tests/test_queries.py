05175bf9fb729377a42026446a5e17e5a0d729b7
Ben Gyori <ben.gyori@gmail.com>
2020-04-15 14:43:36 -0400

    Update grounding test that uses standardization

--- emmaa/tests/test_queries.py
+++ emmaa/tests/test_queries.py
@@ -112,17 +112,20 @@ def test_grounding():
     agent = get_agent_from_text('MAPK1')
     assert isinstance(agent, Agent)
     assert agent.name == 'MAPK1'
-    assert agent.db_refs == {'HGNC': '6871'}
+    assert agent.db_refs == {'TEXT': 'MAPK1',
+                             'HGNC': '6871', 'UP': 'P28482'}, agent.db_refs
     # test with lower case
     agent = get_agent_from_text('mapk1')
     assert isinstance(agent, Agent)
     assert agent.name == 'MAPK1'
-    assert agent.db_refs == {'HGNC': '6871'}
+    assert agent.db_refs == {'TEXT': 'mapk1',
+                             'HGNC': '6871', 'UP': 'P28482'}, agent.db_refs
     # other agent
     agent = get_agent_from_text('BRAF')
     assert isinstance(agent, Agent)
     assert agent.name == 'BRAF'
-    assert agent.db_refs == {'HGNC': '1097'}
+    assert agent.db_refs == {'TEXT': 'BRAF',
+                             'HGNC': '1097', 'UP': 'P15056'}, agent.db_refs
 
 
 def test_agent_from_trips():
