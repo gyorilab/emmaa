4877345a30895a514c3e8dcd1cd77986f5b68ec4
dianakolusheva <dianakolusheva@gmail.com>
2020-06-10 15:37:01 -0400

    Organize test info into table

--- emmaa_service/api.py
+++ emmaa_service/api.py
@@ -498,7 +498,11 @@ def get_model_dashboard(model):
         [('', 'Network on Ndex', ''),
          (f'http://www.ndexbio.org/#/network/{ndex_id}', ndex_id,
           'Click to see network on Ndex')]]
-    test_description = test_stats['test_round_summary'].get('test_description')
+    test_data = test_stats['test_round_summary'].get('test_data')
+    test_info_contents = None
+    if test_data:
+        test_info_contents = [[('', k.capitalize(), ''), ('', v, '')]
+                              for k, v in test_data.items()]
     current_model_types = [mt for mt in ALL_MODEL_TYPES if mt in
                            test_stats['test_round_summary']]
     # Get correct and incorrect curation hashes to pass it per stmt
@@ -542,7 +546,7 @@ def get_model_dashboard(model):
                            stmts_counts=top_stmts_counts,
                            added_stmts=added_stmts,
                            model_info_contents=model_info_contents,
-                           test_description=test_description,
+                           test_info_contents=test_info_contents,
                            model_types=["Test", *[FORMATTED_TYPE_NAMES[mt]
                                                   for mt in
                                                   current_model_types]],
