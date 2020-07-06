53618f03ff67a87dc19d56fc3c7748ba00070f10
dianakolusheva <dianakolusheva@gmail.com>
2020-07-06 12:44:37 -0400

    Update after_update lambda function

--- emmaa/aws_lambda_functions/after_update.py
+++ emmaa/aws_lambda_functions/after_update.py
@@ -13,8 +13,41 @@ import boto3
 import json
 
 
+batch = boto3.client('batch')
+JOB_DEF = 'emmaa_jobdef'
+QUEUE = 'emmaa-models-update-test'
+PROJECT = 'aske'
+BRANCH = 'origin/master'
+now_str = datetime.now().strftime('%Y%m%d')
+
+
+def submit_batch_job(script_command, purpose, job_name, wait_for=None):
+    print(f'Submitting job {job_name}')
+    core_command = 'bash scripts/git_and_run.sh'
+    if BRANCH is not None:
+        core_command += f' --branch {BRANCH}'
+    core_command += script_command
+    print(core_command)
+    cont_overrides = {
+        'command': ['python', '-m', 'indra.util.aws', 'run_in_batch',
+                    '--project', PROJECT, '--purpose', purpose,
+                    core_command]
+        }
+    kwargs = {}
+    if wait_for:
+        kwargs['dependsOn'] = [{'jobId': job_id, 'type': 'SEQUENTIAL'}
+                               for job_id in wait_for]
+    ret = batch.submit_job(
+        jobName=job_name,
+        jobQueue=QUEUE, jobDefinition=JOB_DEF,
+        containerOverrides=cont_overrides, **kwargs)
+    job_id = ret['jobId']
+    print(f"Result from job submission: {job_id}")
+    return job_id
+
+
 def lambda_handler(event, context):
-    """Invoke model stats, test pipeline and query functions.
+    """Submit model tests, model and test stats, and query batch jobs.
 
     This function is designed to be placed on AWS Lambda, taking the event and
     context arguments that are passed. Note that this function must always have
@@ -42,7 +75,6 @@ def lambda_handler(event, context):
         other data to be returned to Lambda.
     """
     s3 = boto3.client('s3')
-    lam = boto3.client('lambda')
     records = event['Records']
     for rec in records:
         try:
@@ -50,19 +82,52 @@ def lambda_handler(event, context):
         except KeyError:
             pass
         model_name = model_key.split('/')[1]
-        payload = {"model": model_name}
-        for func in ['emmaa-model-stats', 'emmaa-test-pipeline',
-                     'emmaa-queries']:
-            resp = lam.invoke(FunctionName=func,
-                              InvocationType='RequestResponse',
-                              Payload=json.dumps(payload))
-            print(resp['Payload'].read())
+
+        # Store all stats jobs IDs
+        job_ids = []
+
+        # Submit model stats job
+        model_stats_command = (' python scripts/run_model_stats_from_s3.py'
+                               f' --model {model_name}  --stats_mode model')
+        model_stats_id = submit_batch_job(
+            model_stats_command, 'update-emmaa-model-stats',
+            f'{model_name}_model_stats_{now_str}')
+        job_ids.append(model_stats_id)
+
+        # Find all test corpora for daily runi
         config_key = f'models/{model_name}/config.json'
         obj = s3.get_object(Bucket='emmaa', Key=config_key)
         config = json.loads(obj['Body'].read().decode('utf8'))
+        tests = config['test'].get('test_corpus', 'large_corpus_tests')
+        if isinstance(tests, str):
+            tests = [tests]
+
+        # For each test run the test and test stats
+        for test in tests:
+            test_command = (' python scripts/run_model_tests_from_s3.py'
+                            f' --model {model_name} --tests {test_corpus}')
+            test_id = submit_batch_job(
+                test_command, 'update-emmaa-results',
+                f'{model_name}_{test_corpus}_tests_{now_str}')
+            test_stats_command = (' python scripts/run_model_stats_from_s3.py'
+                                  f' --model {model_name} --stats_mode tests'
+                                  f' --tests {test_corpus}')
+            test_stats_id = submit_batch_job(
+                test_stats_command, 'update-emmaa-test-stats',
+                f'{model_name}_{test_corpus}_stats_{now_str}', [test_id])
+            job_ids.append(test_stats_id)
+
+        # Run queries
+        query_command = (' python scripts/answer_queries_from_s3.py'
+                         f' --model {model_name}')
+        submit_batch_job(query_command, 'update-emmaa-queries',
+                         f'{model_name}_queries_{now_str}')
+
+        # Make tests if configured
         if config.get('make_tests', False):
-            resp = lam.invoke(FunctionName='emmaa-test-update',
-                              InvocationType='RequestResponse',
-                              Payload=json.dumps(payload))
-            print(resp['Payload'].read())
-    return {'statusCode': 200, 'result': 'SUCCESS'}
+            test_update_command = (' python scripts/model_to_tests.py'
+                                   f' --model {model_name}')
+            submit_batch_job(test_update_command, 'update-emmaa-tests',
+                             f'{model_name}_test_update_{now_str}')
+
+    return 'All jobs sumbitted'
