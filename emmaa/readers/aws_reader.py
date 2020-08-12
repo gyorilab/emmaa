75b7ba0f4e2c12329337218163286c4d4e028241
dianakolusheva <dianakolusheva@gmail.com>
2020-08-12 17:26:54 -0400

    Update imports from indra_reading

--- emmaa/readers/aws_reader.py
+++ emmaa/readers/aws_reader.py
@@ -2,7 +2,8 @@ import datetime
 from indra.sources import reach
 from indra.literature.s3_client import get_reader_json_str, get_full_text
 from indra_reading.scripts.submit_reading_pipeline import \
-    submit_reading, BatchMonitor
+    submit_reading
+from indra_reading.batch.monitor import BatchMonitor
 from emmaa.statements import EmmaaStatement
 
 
