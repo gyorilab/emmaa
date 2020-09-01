34eb4a27bbc6505cf5b43cecdb44d90ae885e271
Ben Gyori <ben.gyori@gmail.com>
2020-09-01 18:06:39 -0400

    Bump version

--- emmaa/__init__.py
+++ emmaa/__init__.py
@@ -1,6 +1,6 @@
 import logging
 
-__version__ = '1.7.0'
+__version__ = '1.8.0'
 
 logging_format = '%(levelname)s: [%(asctime)s] %(name)s - %(message)s'
 logging.basicConfig(format=logging_format, level=logging.INFO,
