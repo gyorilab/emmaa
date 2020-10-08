a6772d5cbb1e88b9d2321b4b26cbe2707813fb6a
dianakolusheva <dianakolusheva@gmail.com>
2020-10-07 21:17:37 -0400

    Turn some methods into functions

--- emmaa/answer_queries.py
+++ emmaa/answer_queries.py
@@ -109,55 +109,6 @@ class QueryManager(object):
             query_hashes, latest_order=latest_order)
         return format_results(results, query_type)
 
-    def make_reports_from_results(self, new_results, domain='emmaa.indra.bio'):
-        """Make a report given latest results and queries the results are for.
-
-        Parameters
-        ----------
-        new_results : list[tuple]
-            Latest results as a list of tuples where each tuple has the format
-            (model_name, query, mc_type, result_json, date, delta).
-
-        Returns
-        -------
-        reports : list
-            A list of reports on changes for each of the queries.
-        """
-        processed_query_mc = []
-        static_reports = []
-        open_reports = []
-        dynamic_reports = []
-        for model_name, query, mc_type, result_json, delta, _ in new_results:
-            if (model_name, query, mc_type) in processed_query_mc:
-                continue
-            if delta:
-                model_type_name = FORMATTED_TYPE_NAMES[
-                    mc_type] if mc_type else mc_type
-                rep = [
-                    query.to_english(),
-                    _detailed_page_link(
-                        domain,
-                        model_name,
-                        mc_type,
-                        query.get_hash_with_model(
-                            model_name)),
-                    model_name,
-                    model_type_name
-                ]
-                # static
-                if query.get_type() == 'path_property':
-                    static_reports.append(rep)
-                # open
-                elif query.get_type() == 'open_search_query':
-                    open_reports.append(rep)
-                # dynamic
-                else:
-                    # Remove link for dynamic
-                    _ = rep.pop(1)
-                    dynamic_reports.append(rep)
-            processed_query_mc.append((model_name, query, mc_type))
-        return static_reports, open_reports, dynamic_reports
-
     def get_user_query_delta(self, user_email, domain='emmaa.indra.bio'):
         """Produce a report for all query results per user in a given format
 
@@ -180,19 +131,19 @@ class QueryManager(object):
 
         # Get the query deltas
         static_results_delta, open_results_delta, dynamic_results_delta = \
-            self.make_reports_from_results(results, domain=domain)
+            make_reports_from_results(results, domain=domain)
         # Make text report
-        str_report = self.make_str_report_per_user(static_results_delta,
-                                                   open_results_delta,
-                                                   dynamic_results_delta)
+        str_report = make_str_report_per_user(static_results_delta,
+                                              open_results_delta,
+                                              dynamic_results_delta)
         str_report = str_report if str_report else ''
 
         # Make html report
-        html_report = self.make_html_report_per_user(static_results_delta,
-                                                     open_results_delta,
-                                                     dynamic_results_delta,
-                                                     user_email,
-                                                     domain=domain)
+        html_report = make_html_report_per_user(static_results_delta,
+                                                open_results_delta,
+                                                dynamic_results_delta,
+                                                user_email,
+                                                domain=domain)
         html_report = html_report if html_report else None
 
         if html_report:
@@ -201,92 +152,145 @@ class QueryManager(object):
             logger.info(f'No query delta to report for {user_email}')
         return str_report, html_report
 
-    def make_str_report_per_user(self, static_results_delta,
-                                 open_results_delta, dynamic_results_delta):
-        """Produce a report for all query results per user as a string.
-
-        Parameters
-        ----------
-        static_results_delta : list
-            A list of tuples of query deltas for static queries. Each tuple
-            has a format (english_query, link, model, mc_type)
-        open_results_delta : list
-            A list of tuples of query deltas for open queries. Each tuple
-            has a format (english_query, link, model, mc_type)
-        dynamic_results_delta : list
-            A list of tuples of query deltas for dynamic queries. Each tuple
-            has a format (english_query, link, model, mc_type)
-
-        Returns
-        -------
-        msg : str
-            A message about query deltas.
-        """
-        if not static_results_delta and not open_results_delta and not \
-                dynamic_results_delta:
-            logger.info('No delta provided')
-            return None
-        msg = ''
-        if static_results_delta:
-            msg += 'Updates to your static queries:\n'
-            for english_query, _, model, mc_type in static_results_delta:
-                msg += f'{english_query} in {model} using the {mc_type}.\n'
-        if open_results_delta:
-            msg += 'Updates to your open queries:\n'
-            for english_query, _, model, mc_type in open_results_delta:
-                msg += f'{english_query} in {model} using the {mc_type}.\n'
-        if dynamic_results_delta:
-            msg += 'Updates to your dynamic queries:\n'
-            for english_query, _, model, mc_type in dynamic_results_delta:
-                msg += f'{english_query} in {model} using the {mc_type}.\n'
-        return msg
-
-    def make_html_report_per_user(self, static_results_delta,
-                                  open_results_delta, dynamic_results_delta,
-                                  email, domain='emmaa.indra.bio'):
-        """Produce a report for all query results per user in an html file.
-
-        Parameters
-        ----------
-        static_results_delta : list
-            A list of tuples of query deltas for static queries. Each tuple
-            has a format (english_query, link, model, mc_type)
-        open_results_delta : list
-            A list of tuples of query deltas for open queries. Each tuple
-            has a format (english_query, link, model, mc_type)
-        dynamic_results_delta : list
-            A list of tuples of query deltas for dynamic queries. Each tuple
-            has a format (english_query, link, model, mc_type)
-        email : str
-            The email of the user to get the results for.
-        domain : str
-            The domain name for the unsubscibe link in the report. Default:
-            "emmaa.indra.bio".
-
-        Returns
-        -------
-        str
-            A string containing an html document
-        """
-        # Generate unsubscribe link
-        link = generate_unsubscribe_link(email=email, domain=domain)
-
-        if static_results_delta or open_results_delta or dynamic_results_delta:
-            return email_html.render(
-                static_query_deltas=static_results_delta,
-                open_query_deltas=open_results_delta,
-                dynamic_query_deltas=dynamic_results_delta,
-                unsub_link=link
-            )
-        else:
-            return ''
-
     def get_model_manager(self, model_name):
         # Try get model manager from class attributes or load from s3.
         for mm in self.model_managers:
             if mm.model.name == model_name:
                 return mm
         return load_model_manager_from_cache(model_name)
+
+
+def make_reports_from_results(new_results, domain='emmaa.indra.bio'):
+    """Make a report given latest results and queries the results are for.
+
+    Parameters
+    ----------
+    new_results : list[tuple]
+        Latest results as a list of tuples where each tuple has the format
+        (model_name, query, mc_type, result_json, date, delta).
+
+    Returns
+    -------
+    reports : list
+        A list of reports on changes for each of the queries.
+    """
+    processed_query_mc = []
+    static_reports = []
+    open_reports = []
+    dynamic_reports = []
+    for model_name, query, mc_type, result_json, delta, _ in new_results:
+        if (model_name, query, mc_type) in processed_query_mc:
+            continue
+        if delta:
+            model_type_name = FORMATTED_TYPE_NAMES[
+                mc_type] if mc_type else mc_type
+            rep = [
+                query.to_english(),
+                _detailed_page_link(
+                    domain,
+                    model_name,
+                    mc_type,
+                    query.get_hash_with_model(
+                        model_name)),
+                model_name,
+                model_type_name
+            ]
+            # static
+            if query.get_type() == 'path_property':
+                static_reports.append(rep)
+            # open
+            elif query.get_type() == 'open_search_query':
+                open_reports.append(rep)
+            # dynamic
+            else:
+                # Remove link for dynamic
+                _ = rep.pop(1)
+                dynamic_reports.append(rep)
+        processed_query_mc.append((model_name, query, mc_type))
+    return static_reports, open_reports, dynamic_reports
+
+
+def make_str_report_per_user(static_results_delta, open_results_delta,
+                             dynamic_results_delta):
+    """Produce a report for all query results per user as a string.
+
+    Parameters
+    ----------
+    static_results_delta : list
+        A list of tuples of query deltas for static queries. Each tuple
+        has a format (english_query, link, model, mc_type)
+    open_results_delta : list
+        A list of tuples of query deltas for open queries. Each tuple
+        has a format (english_query, link, model, mc_type)
+    dynamic_results_delta : list
+        A list of tuples of query deltas for dynamic queries. Each tuple
+        has a format (english_query, link, model, mc_type) (no link in
+        dynamic_results_delta tuples).
+
+    Returns
+    -------
+    msg : str
+        A message about query deltas.
+    """
+    if not static_results_delta and not open_results_delta and not \
+            dynamic_results_delta:
+        logger.info('No delta provided')
+        return None
+    msg = ''
+    if static_results_delta:
+        msg += 'Updates to your static queries:\n'
+        for english_query, _, model, mc_type in static_results_delta:
+            msg += f'{english_query} in {model} using the {mc_type}.\n'
+    if open_results_delta:
+        msg += 'Updates to your open queries:\n'
+        for english_query, _, model, mc_type in open_results_delta:
+            msg += f'{english_query} in {model} using the {mc_type}.\n'
+    if dynamic_results_delta:
+        msg += 'Updates to your dynamic queries:\n'
+        for english_query, model, mc_type in dynamic_results_delta:
+            msg += f'{english_query} in {model} using the {mc_type}.\n'
+    return msg
+
+
+def make_html_report_per_user(static_results_delta, open_results_delta,
+                              dynamic_results_delta, email,
+                              domain='emmaa.indra.bio'):
+    """Produce a report for all query results per user in an html file.
+
+    Parameters
+    ----------
+    static_results_delta : list
+        A list of tuples of query deltas for static queries. Each tuple
+        has a format (english_query, link, model, mc_type)
+    open_results_delta : list
+        A list of tuples of query deltas for open queries. Each tuple
+        has a format (english_query, link, model, mc_type)
+    dynamic_results_delta : list
+        A list of tuples of query deltas for dynamic queries. Each tuple
+        has a format (english_query, link, model, mc_type)
+    email : str
+        The email of the user to get the results for.
+    domain : str
+        The domain name for the unsubscibe link in the report. Default:
+        "emmaa.indra.bio".
+
+    Returns
+    -------
+    str
+        A string containing an html document
+    """
+    # Generate unsubscribe link
+    link = generate_unsubscribe_link(email=email, domain=domain)
+
+    if static_results_delta or open_results_delta or dynamic_results_delta:
+        return email_html.render(
+            static_query_deltas=static_results_delta,
+            open_query_deltas=open_results_delta,
+            dynamic_query_deltas=dynamic_results_delta,
+            unsub_link=link
+        )
+    else:
+        return ''
 
 
 def _detailed_page_link(domain, model_name, model_type, query_hash):
