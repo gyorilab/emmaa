48b022210603b32ee3f63a4cb41fd15f89ab9b47
dianakolusheva <dianakolusheva@gmail.com>
2020-10-08 10:37:29 -0400

    Add more db tests

--- emmaa/tests/test_db.py
+++ emmaa/tests/test_db.py
@@ -4,7 +4,7 @@ import random
 from nose.plugins.attrib import attr
 from nose.tools import with_setup
 
-from emmaa.db import Query, Result
+from emmaa.db import Query, Result, User, UserQuery
 from emmaa.queries import Query as QueryObject, PathProperty
 from emmaa.tests.db_setup import _get_test_db, setup_function, \
     teardown_function
@@ -37,6 +37,11 @@ def test_put_queries():
     with db.get_session() as sess:
         queries = sess.query(Query).all()
     assert len(queries) == 2, len(queries)
+    # Not logged in user
+    db.put_queries(None, None, test_queries[1], ['aml'])
+    with db.get_session() as sess:
+        queries = sess.query(Query).all()
+    assert len(queries) == 3, len(queries)
 
 
 @with_setup(setup_function, teardown_function)
@@ -124,3 +129,86 @@ def test_get_latest_results():
     assert any(results[0][1].matches(q) for q in test_queries)
     assert all(isinstance(result[2], str) for result in results)
     assert all(isinstance(result[3], dict) for result in results)
+
+
+@with_setup(setup_function, teardown_function)
+@attr('nonpublic')
+def test_get_subscribed_queries():
+    db = _get_test_db()
+    db.put_queries('test@test.com', 1, test_queries[0], ['aml'], True)
+    db.put_queries('test@test.com', 1, test_queries[1], ['aml'], False)
+    # Only return queries for which subscription is True
+    queries = db.get_subscribed_queries('test@test.com')
+    assert len(queries) == 1
+    assert queries[0][2] == test_queries[0].get_hash_with_model('aml')
+
+
+@with_setup(setup_function, teardown_function)
+@attr('nonpublic')
+def test_get_subscribed_users():
+    db = _get_test_db()
+    db.put_queries('test1@test.com', 1, test_queries[0], ['aml'], True)
+    db.put_queries('test2@test.com', 2, test_queries[1], ['aml'], False)
+    # Only return users that subscribed for something
+    emails = db.get_subscribed_users()
+    assert len(emails) == 1
+    assert emails[0] == 'test1@test.com'
+
+
+@with_setup(setup_function, teardown_function)
+@attr('nonpublic')
+def test_update_email_subscription():
+    db = _get_test_db()
+    db.put_queries('test1@test.com', 1, test_queries[0], ['aml'], True)
+    qh = test_queries[0].get_hash_with_model('aml')
+    with db.get_session() as sess:
+        q = sess.query(UserQuery.subscription).filter(
+            UserQuery.user_id == 1, UserQuery.query_hash == qh)
+    assert [q[0] for q in q.all()][0]  # True
+    db.update_email_subscription('test1@test.com', [qh], False)
+    with db.get_session() as sess:
+        q = sess.query(UserQuery.subscription).filter(
+            UserQuery.user_id == 1,
+            UserQuery.query_hash == test_queries[0].get_hash_with_model('aml'))
+    assert not [q[0] for q in q.all()][0]  # new subscription status is False
+
+
+@with_setup(setup_function, teardown_function)
+@attr('nonpublic')
+def test_get_number_results():
+    db = _get_test_db()
+    db.put_queries('test@test.com', 1, test_queries[0], ['aml'])
+    db.put_results('aml', [(test_queries[0], 'pysb', _get_random_result())])
+    time.sleep(1)
+    db.put_results('aml', [(test_queries[0], 'pysb', _get_random_result())])
+    time.sleep(1)
+    db.put_results('aml', [(test_queries[0], 'pysb', _get_random_result())])
+    qh = test_queries[0].get_hash_with_model('aml')
+    assert db.get_number_of_results(qh, 'pysb') == 3
+
+
+@with_setup(setup_function, teardown_function)
+@attr('nonpublic')
+def test_get_all_result_hashes_and_delta():
+    db = _get_test_db()
+    db.put_queries('test@test.com', 1, test_queries[0], ['aml'])
+    qh = test_queries[0].get_hash_with_model('aml')
+    # If there are no older results, all hashes is None
+    assert db.get_all_result_hashes(qh, 'pysb') is None
+    db.put_results('aml', [(test_queries[0], 'pysb', {'1234': 'result'})])
+    assert db.get_all_result_hashes(qh, 'pysb') == {'1234'}
+    # First result is not delta
+    results = db.get_results_from_hashes([qh])
+    assert results[0][4] == [], results[0]
+    # All hashes keeps growing
+    time.sleep(1)
+    db.put_results('aml', [(test_queries[0], 'pysb', {'345': 'other'})])
+    assert db.get_all_result_hashes(qh, 'pysb') == {'1234', '345'}
+    results = db.get_results_from_hashes([qh])
+    assert results[0][4] == ['345'], results[0]
+    # When we go to previous result, it's not delta
+    time.sleep(1)
+    db.put_results('aml', [(test_queries[0], 'pysb', {'1234': 'result'})])
+    assert db.get_all_result_hashes(qh, 'pysb') == {'1234', '345'}
+    results = db.get_results_from_hashes([qh])
+    assert results[0][4] == [], results[0]
