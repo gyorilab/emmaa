592cab480e71bb1074052889761f42ffe808021e
dianakolusheva <dianakolusheva@gmail.com>
2020-10-14 13:12:24 -0400

    Add endpoint to get latest date

--- emmaa_service/api.py
+++ emmaa_service/api.py
@@ -1237,6 +1237,37 @@ def get_latest_statements_url(model):
     return {'link': link}
 
 
+@app.route('/latest_date/', methods=['GET'])
+def get_latest_date():
+    """Return latest available date of model and test stats.
+
+    Parameters
+    ----------
+    model : str
+        Name of the model.
+    test_corpus : Optional[str]
+        Which test corpus stats to check. If not provided, default test corpus
+        for the model is used.
+    date_format : Optional[str]
+        Which format of the date to return: 'date' or 'datetime'. Default:
+        datetime.
+
+    Returns
+    -------
+    json : dict
+        A dictionary with key 'date' and value of a latest available date in a
+        selected format.
+    """
+    model = request.json.get('model')
+    if not model:
+        abort(Response('Need to provide model', 404))
+    date_format = request.json.get('date_format', 'datetime')
+    test_corpus = request.json.get('test_corpus', _default_test(model))
+    date = get_latest_available_date(
+        model, test_corpus, date_format=date_format, bucket=EMMAA_BUCKET_NAME)
+    return jsonify({'date': date})
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('Run the EMMAA dashboard service.')
     parser.add_argument('--host', default='0.0.0.0')
