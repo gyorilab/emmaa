aca0fd29766c9eabe49dcd4ddcc77d63c2bd709a
John Bachman <bachmanjohn@gmail.com>
2020-10-27 12:11:13 -0400

    Clean up cruft from gordon stmts script

--- models/covid19/gordon_subnet.py
+++ models/covid19/gordon_subnet.py
@@ -8,10 +8,8 @@ from indra.sources import indra_db_rest as idr
 stmts_path = join(dirname(abspath(__file__)), '..', '..', '..',
                          'covid-19', 'stmts')
 gordon_stmts_path = join(stmts_path, 'gordon_ndex_stmts.pkl')
-combined_stmts_path = join(stmts_path, 'cord19_combined_stmts.pkl')
 
 gordon_stmts = ac.load_statements(gordon_stmts_path)
-#comb_stmts = ac.load_statements(combined_stmts_path)
 
 # Get human interactors of viral proteins from Gordon et al.
 hgnc_ids = [ag.db_refs['HGNC'] for stmt in gordon_stmts
@@ -26,23 +24,6 @@ for gene in hgnc_names:
 
 ac.dump_statements(stmts, 'gordon_ppi_stmts.pkl')
 
-"""
-# Filter statements to only those involving at least one of the given
-# genes
-filt_stmts = ac.filter_gene_list(comb_stmts, hgnc_names, policy='all',
-                                 allow_families=True)
-filt_stmts = [s for s in filt_stmts if s.agent_list()[0] is not None]
-
-# Add back the Gordon et al. statements
-all_stmts = filt_stmts + gordon_stmts
-
-ina = IndraNetAssembler(all_stmts)
-dg = ina.make_model(graph_type='digraph')
-nx_to_graph_commons(dg, 'Gordon', 'gordon_gc.json',
-                    graph_description="SARS-Cov-2 PPIs")
-# Save combined statements
-ac.dump_statements(all_stmts, 'gordon_stmts.pkl')
-"""
 
 if __name__ == '__main__':
     pass
