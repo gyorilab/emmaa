c00bb4c84fcbc1dbca4922f37f93c6b0c4eaa5c4
dianakolusheva <dianakolusheva@gmail.com>
2021-02-02 18:23:10 -0500

    Update use of get_curations in api

--- emmaa_service/api.py
+++ emmaa_service/api.py
@@ -24,8 +24,8 @@ from indra.statements import get_all_descendants, IncreaseAmount, \
     RemoveModification, get_statement_by_name, stmts_to_json
 from indra.assemblers.html.assembler import _format_evidence_text, \
     _format_stmt_text
-from indra.sources.indra_db_rest import submit_curation
-from indra_db.client.principal.curation import get_curations
+from indra.sources.indra_db_rest import submit_curation, get_curations, \
+    get_all_curations
 
 from emmaa.util import find_latest_s3_file, does_exist, \
     EMMAA_BUCKET_NAME, list_s3_files, find_index_of_s3_file, \
@@ -472,7 +472,7 @@ def _set_curation(stmt_hash, correct, incorrect):
 
 def _label_curations(include_partial=False, **kwargs):
     logger.info('Getting curations')
-    curations = get_curations(**kwargs)
+    curations = get_all_curations()
     logger.info('Labeling curations')
     if include_partial:
         correct = {str(c['pa_hash']) for c in curations if
@@ -955,7 +955,9 @@ def get_paper_statements(model):
     stmts_by_hash = {}
     for stmt in updated_stmts:
         stmts_by_hash[str(stmt.get_hash())] = stmt
-    curations = get_curations(pa_hash=paper_hashes)
+    all_curations = get_all_curations()
+    curations = [
+        cur for cur in all_curations if cur['pa_hash'] in paper_hashes]
     cur_dict = defaultdict(list)
     for cur in curations:
         cur_dict[(cur['pa_hash'], cur['source_hash'])].append(
@@ -1281,7 +1283,9 @@ def get_statement_evidence_page():
         stmts_by_hash = {}
         for stmt in stmts:
             stmts_by_hash[str(stmt.get_hash())] = stmt
-        curations = get_curations(pa_hash=stmt_hashes)
+        all_curations = get_all_curations()
+        curations = [
+            cur for cur in all_curations if cur['pa_hash'] in stmt_hashes]
         cur_dict = defaultdict(list)
         for cur in curations:
             cur_dict[(cur['pa_hash'], cur['source_hash'])].append(
@@ -1348,7 +1352,7 @@ def get_all_statements_page(model):
     for stmt in stmts:
         stmts_by_hash[str(stmt.get_hash())] = stmt
     msg = None
-    curations = get_curations()
+    curations = get_all_curations()
     cur_counts = _count_curations(curations, stmts_by_hash)
     if filter_curated:
         stmts = [stmt for stmt in stmts if str(stmt.get_hash()) not in
@@ -1652,7 +1656,8 @@ def get_statement_by_hash_model(model, date, hash_val):
     """Get model statement JSON by hash."""
     stmts = _load_stmts_from_cache(model, date)
     st_json = {}
-    curations = get_curations(pa_hash=hash_val)
+    all_curations = get_all_curations()
+    curations = [cur for cur in all_curations if cur['pa_hash'] == hash_val]
     cur_dict = defaultdict(list)
     for cur in curations:
         cur_dict[(cur['pa_hash'], cur['source_hash'])].append(
@@ -1670,7 +1675,8 @@ def get_statement_by_hash_model(model, date, hash_val):
 def get_tests_by_hash(test_corpus, hash_val):
     """Get test statement JSON by hash."""
     tests = _load_tests_from_cache(test_corpus)
-    curations = get_curations(pa_hash=hash_val)
+    all_curations = get_all_curations()
+    curations = [cur for cur in all_curations if cur['pa_hash'] == hash_val]
     cur_dict = defaultdict(list)
     for cur in curations:
         cur_dict[(cur['pa_hash'], cur['source_hash'])].append(
@@ -1691,7 +1697,8 @@ def get_statement_by_paper(model, paper_id, paper_id_type, date, hash_val):
     """Get model statement by hash and paper ID."""
     stmts = _load_stmts_from_cache(model, date)
     st_json = {}
-    curations = get_curations(pa_hash=hash_val)
+    all_curations = get_all_curations()
+    curations = [cur for cur in all_curations if cur['pa_hash'] == hash_val]
     cur_dict = defaultdict(list)
     for cur in curations:
         cur_dict[(cur['pa_hash'], cur['source_hash'])].append(
@@ -1744,7 +1751,7 @@ def submit_curation_endpoint(hash_val, **kwargs):
 
 @app.route('/curation/list/<stmt_hash>/<src_hash>', methods=['GET'])
 def list_curations(stmt_hash, src_hash):
-    curations = get_curations(pa_hash=stmt_hash, source_hash=src_hash)
+    curations = get_curations(hash_val=stmt_hash, source_hash=src_hash)
     return jsonify(curations)
 
 
