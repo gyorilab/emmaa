2409b605429e836d9249dff922a3fb36f2aed48f
dianakolusheva <dianakolusheva@gmail.com>
2021-02-18 16:35:20 -0500

    Make style changes

--- emmaa_service/static/widget/js/app.js
+++ emmaa_service/static/widget/js/app.js
@@ -19,7 +19,6 @@
 
     const chatPage   = $(document)
     const chatWindow = $('.chatbubble')
-    const chatHeader = chatWindow.find('.unexpanded')
     const chatBody   = chatWindow.find('.chat-window')
 
 
@@ -28,17 +27,6 @@
     // ----------------------------------------------------
 
     let helpers = {
-
-    // ----------------------------------------------------
-    // Toggles the display of the chat window.
-    // ----------------------------------------------------
-
-        ToggleChatWindow: function () {
-            chatWindow.toggleClass('opened')
-            chatHeader.find('.title').text(
-                chatWindow.hasClass('opened') ? 'Minimize Chat' : 'Chat with CLARE'
-            )
-        },
     // --------------------------------------------------------------------
     // Show the appropriate display screen. Login screen or Chat screen.
     // --------------------------------------------------------------------
@@ -166,7 +154,7 @@
     // ----------------------------------------------------
 
     chatPage.ready(helpers.ShowAppropriateChatDisplay)
-    chatHeader.on('click', helpers.ToggleChatWindow)
+    chatWindow.toggleClass('opened')
     chatBody.find('#loginScreenForm').on('submit', helpers.LogIntoChatSession)
     chatBody.find('#messageSupport').on('submit', helpers.SendMessageToSupport)
 }())
