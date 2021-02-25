7163e8f9bbcf7a1dbc266f8a59bf46ff510febc0
dianakolusheva <dianakolusheva@gmail.com>
2021-02-25 17:06:54 -0500

    Implement paging for document search

--- emmaa/xdd/xdd_client.py
+++ emmaa/xdd/xdd_client.py
@@ -13,16 +13,25 @@ query_url = 'https://xdd.wisc.edu/sets/xdd-covid-19/cosmos/api/search'
 
 def get_document_objects(doi):
     """Get a list of figure/table object dictionaries for a given DOI."""
-    res = requests.get(doc_url, params={'doi': doi, 'api_key': api_key})
-    rj = res.json()
-    if 'objects' not in rj:
-        logger.warning(f'Could not get objects for {doi}')
-        if 'error' in rj:
-            logger.warning(rj['error'])
-        return
-    objects = [
-        obj for obj in rj['objects'] if obj['cls'] in ['Figure', 'Table']]
-    return objects
+    logger.info(f'Got a request to get figures for DOI {doi}')
+    # Get first batch of results and find the total number of results
+    rj = send_document_search_request(doi, page=0)
+    if not rj:
+        return []
+    total = rj.get('total', 0)
+    logger.info(f'Got a total of {total} objects')
+    objects = rj['objects']
+    page = 0
+    while len(objects) < total:
+        page += 1
+        rj = send_document_search_request(doi, page=page)
+        if not rj:
+            logger.warning(f'Did not get results for {doi} page {page}')
+            break
+        objects += rj['objects']
+    filtered_objects = [
+        obj for obj in objects if obj['cls'] in ['Figure', 'Table']]
+    return filtered_objects
 
 
 def get_figure_from_document_object(obj_dict):
@@ -126,12 +135,9 @@ def get_figures_from_query(query, limit=None):
     return figures
 
 
-def send_query_search_request(query, page):
-    """Send a request to get one page of results for a query."""
-    logger.info(f'Sending a request for query {query}, page {page}')
-    res = requests.get(
-        query_url,
-        params={'query': query, 'inclusive': True, 'page': page})
+def send_request(url, params):
+    """Send a request and handle potential errors."""
+    res = requests.get(url, params=params)
     try:
         rj = res.json()
         if 'objects' not in rj:
@@ -145,9 +151,23 @@ def send_query_search_request(query, page):
     return rj
 
 
+def send_query_search_request(query, page):
+    """Send a request to get one page of results for a query."""
+    logger.info(f'Sending a request for query {query}, page {page}')
+    return send_request(query_url,
+                        {'query': query, 'inclusive': True, 'page': page})
+
+
+def send_document_search_request(doi, page):
+    """Send a request to get one page of results for a DOI."""
+    logger.info(f'Sending a request for DOI {doi}, page {page}')
+    return send_request(doc_url,
+                        {'doi': doi, 'api_key': api_key, 'page': page})
+
+
 def get_figures_from_query_objects(objects):
     """Get a list of figure titles and their content bytes from a list of
-    object dictionaries (returned from query api).
+    object dictionaries (returned from query api)."""
     figures = []
     for obj in objects:
         for child in obj['children']:
