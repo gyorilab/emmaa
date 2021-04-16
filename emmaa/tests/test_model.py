e87a704931f44d4d2b7be1663228d3723bbaa2cc
dianakolusheva <dianakolusheva@gmail.com>
2021-04-16 15:08:57 -0400

    Fix model tests

--- emmaa/tests/test_model.py
+++ emmaa/tests/test_model.py
@@ -60,14 +60,14 @@ def test_model_extend():
     ev3 = Evidence(pmid='1234', text='abcd', source_api='x')
     indra_sts = [Phosphorylation(None, Agent('a'), evidence=ev) for ev in
                  [ev1, ev2, ev3]]
-    emmaa_sts = [EmmaaStatement(st, datetime.datetime.now(), ['x']) for st in
+    emmaa_sts = [EmmaaStatement(st, datetime.datetime.now(), []) for st in
                  indra_sts]
     em = EmmaaModel('x', {'search_terms': [], 'ndex': {'network': None}})
     em.add_statements([emmaa_sts[0]])
     em.extend_unique(emmaa_sts[1:])
     assert len(em.stmts) == 2
     stmt = EmmaaStatement(Phosphorylation(None, Agent('b'), evidence=ev1),
-                          datetime.datetime.now(), ['x'])
+                          datetime.datetime.now(), [])
     em.extend_unique([stmt])
     assert len(em.stmts) == 3
 
