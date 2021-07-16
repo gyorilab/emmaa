7a6fff824615b7e985c8a6200ff3870db379ff69
dianakolusheva <dianakolusheva@gmail.com>
2021-07-16 19:27:30 -0400

    Add belief range

--- emmaa_service/static/emmaaFunctions.js
+++ emmaa_service/static/emmaaFunctions.js
@@ -793,3 +793,33 @@ function generateLineArea(chartDivId, dataParams, chartTitle, yticks=null, forma
     }
   });
 }
+
+// Functions for belief range update
+// Source: https://stackoverflow.com/questions/4753946/html5-slider-with-two-inputs-possible
+function getVals(){
+  // Get slider values
+  var parent = this.parentNode;
+  var slides = parent.getElementsByTagName("input");
+    var slide1 = parseFloat( slides[0].value );
+    var slide2 = parseFloat( slides[1].value );
+  // Neither slider will clip the other, so make sure we determine which is larger
+  if( slide1 > slide2 ){ var tmp = slide2; slide2 = slide1; slide1 = tmp; }
+  
+  var displayElement = parent.getElementsByClassName("rangeValues")[0];
+      displayElement.innerHTML = slide1 + " - " + slide2;
+}
+
+window.onload = function(){
+  // Initialize Sliders
+  var sliderSections = document.getElementsByClassName("range-slider");
+      for( var x = 0; x < sliderSections.length; x++ ){
+        var sliders = sliderSections[x].getElementsByTagName("input");
+        for( var y = 0; y < sliders.length; y++ ){
+          if( sliders[y].type ==="range" ){
+            sliders[y].oninput = getVals;
+            // Manually trigger event first time to display values
+            sliders[y].oninput();
+          }
+        }
+      }
+}
