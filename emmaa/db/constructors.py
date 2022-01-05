a86491c6926d667cb79077bd344def547c11888c
dianakolusheva <dianakolusheva@gmail.com>
2022-01-05 11:37:50 -0500

    Allow db host urls without names

--- emmaa/db/constructors.py
+++ emmaa/db/constructors.py
@@ -12,7 +12,7 @@ def get_db(name):
     """Get a db instance based on its name in the config or env."""
     defaults = get_databases()
     db_name, db_type = defaults[name]
-    m = re.match('(\w+)://.*?/([\w.]+)', db_name)
+    m = re.match('(\w+)://.*?/*([\w.]+)', db_name)
     if m is None:
         logger.error("Poorly formed db name: %s" % db_name)
         return
