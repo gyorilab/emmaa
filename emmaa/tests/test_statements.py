53006790556b650b679f921cd789d3e00c79c4ba
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-04-05 11:53:46 +0200

    Update test_statements.py

--- emmaa/tests/test_statements.py
+++ emmaa/tests/test_statements.py
@@ -76,7 +76,7 @@ def test_filter_indra_stmts():
     stmt6 = make_stmt_with_evid_anns([False])  # Only false anns
     stmt7 = make_stmt_with_evid_anns([None, False])  # Filter false or unknown
     stmt8 = make_stmt_with_evid_anns([False, False])  # Only false anns
-
+    # Case in which the "internal" key is missing
     stmt9 = make_stmt_with_evid_anns([False])
     del stmt9.evidence[0].annotations["emmaa"]["metadata"]["internal"]
 
@@ -97,5 +97,6 @@ def test_filter_indra_stmts():
     assert stmt6 not in filtered_all
     assert stmt7 not in filtered_all
     assert stmt8 not in filtered_all
+    assert stmt9 not in filtered_all
     # Mixed is filtered too here
     assert stmt5 not in filtered_all
