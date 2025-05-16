974decfe7e00d96d01c3771fb6f32e4eddacef65
kkaris <karis.klas@gmail.com>
2025-05-16 12:56:31 -0700

    Do signed s3 client by default, simplify function.

--- emmaa/util.py
+++ emmaa/util.py
@@ -225,12 +225,10 @@ def does_exist(bucket, prefix, extension=None):
     extension exist in a bucket.
     """
     all_files = list_s3_files(bucket, prefix, extension)
-    if any(fname.startswith(prefix) for fname in all_files):
-        return True
-    return False
+    return any(fname.startswith(prefix) for fname in all_files)
 
 
-def get_s3_client(unsigned=True):
+def get_s3_client(unsigned=False):
     """Return a boto3 S3 client with optional unsigned config.
 
     Parameters
